f080569fa100513b5c0fe770bf002ad7e54d771b
DanrleySA <danrley.alencar@gmail.com>
2017-04-24 11:26:18 -0300

    DatePicker e TimePicker cadastro de eventos.

--- app/src/main/java/danrleysa/com/showtime/controller/Fragment/CadEventoFragment.java
+++ app/src/main/java/danrleysa/com/showtime/controller/Fragment/CadEventoFragment.java
@@ -2,10 +2,12 @@ package danrleysa.com.showtime.controller.Fragment;
 
 
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import danrleysa.com.showtime.R;
 
@@ -14,7 +16,6 @@ import danrleysa.com.showtime.R;
  * A simple {@link Fragment} subclass.
  */
 public class CadEventoFragment extends Fragment {
-
 
     public CadEventoFragment() {
         // Required empty public constructor
@@ -26,8 +27,36 @@ public class CadEventoFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cad_evento, container, false);
 
+        Button btnTime = (Button) view.findViewById(R.id.CadEventoBtnTime);
+        Button btnDate = (Button) view.findViewById(R.id.CadEventoBtnData);
+        Button btnCancel = (Button) view.findViewById(R.id.CadEventoBtnCancel);
+
+        btnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new TimePickerFragment();
+                newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+            }
+        });
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getActivity().getSupportFragmentManager(), "datePicker");
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frameContainerPrincipal, new LoginFragment())
+                        .commit();
+            }
+        });
+
 
         return view;
     }
-
 }
